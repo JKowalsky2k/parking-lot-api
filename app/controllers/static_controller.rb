9acce1b7a9305c68359953ecab9e0ca21cebf363
Jedrzej <rbxxx2k@gmail.com>
2022-06-09 18:51:13 +0200

    added swagger to static_controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,16 @@
 class StaticController < ApplicationController
     before_action :require_token, only: [:summary]
-    def index
-    end
+    swagger_controller :static, 'Static'
 
+    def index
+        @parking_slots = ParkingSlot.all
+        @users = User.all
+    end
+    
+    swagger_api :summary do
+        summary "Shows a summary for authenticated user"
+        param :header, "Authorization", :string, :required, "Authentication token"
+      end
     def summary
     end
 end
